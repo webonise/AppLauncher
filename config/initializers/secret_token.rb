7c6ab43a1f9f6d99fbeae716105b1a6d3f11b7dc
shardul mohite <shardul@weboniselab.com>
2012-03-27 16:44:09 +0530

    First Commit of AppLauncher

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AppLauncher::Application.config.secret_token = '6d514b9c82be245de0afa774174e4864e045ce467ca0f88a46ae456402001a40c86d2cd10dd927bc60d3d5ee37a5e25a1a470a515d2bfeb29f2dea12cbe519b8'
