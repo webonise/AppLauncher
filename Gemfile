123f73a96265804f0082ee89505a38f88f5e1bc8
shardul mohite <shardul@weboniselab.com>
2012-03-27 21:12:40 +0530

    Bootstrap added with Application layout set to work with bootstrap

--- Gemfile
+++ Gemfile
@@ -14,6 +14,8 @@ group :assets do
   gem 'sass-rails',   '~> 3.1.5'
   gem 'coffee-rails', '~> 3.1.1'
   gem 'uglifier', '>= 1.0.3'
+  gem 'twitter-bootstrap-rails','2.0.4'
+
 end
 
 gem 'jquery-rails'
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20120327143730) do
+
+  create_table "projects", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
