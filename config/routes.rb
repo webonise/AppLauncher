3d99da76affe09df8cd226fd20c242c019c5bd2b
shardul mohite <shardul@weboniselab.com>
2012-03-29 12:09:50 +0530

    changed routes files for roots to set to projects index

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ AppLauncher::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'projects#index'
 
   # See how all your routes lay out with "rake routes"
 
